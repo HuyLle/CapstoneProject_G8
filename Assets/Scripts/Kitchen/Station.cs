3f161919e38f0aa895cc368b21bdd98236666e34
HuyLle <lehuy23701@gmail.com>
2025-06-30 21:15:24 +0700

    them local_coop

---
+++ Assets/Scripts/Kitchen/Station.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Station : MonoBehaviour
+{
+    private GameObject currentItem = null; // Vật thể hiện tại trên Station
+
+    public bool IsEmpty => currentItem == null; // Kiểm tra Station trống
+
+    public bool TryPlaceItem(GameObject item)
+    {
+        if (IsEmpty)
+        {
+            currentItem = item;
+            return true;
+        }
+        return false;
+    }
+
+    public void ClearItem()
+    {
+        currentItem = null;
+    }
+
+    public GameObject GetCurrentItem()
+    {
+        return currentItem;
+    }
+}
